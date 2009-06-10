7209fba8ce8fcb3f2fa11cb3588021f892600573
dkm <dkm@gre.kataplop.net>
2009-06-10 21:43:14 +0200

    tracktest format. Used to simulate realtime tracking "input".

--- vecs.c
+++ vecs.c
@@ -134,6 +134,7 @@ extern ff_vecs_t random_vecs;
 extern ff_vecs_t xol_vecs;
 extern ff_vecs_t navilink_vecs;
 extern ff_vecs_t pykml_vecs;
+extern ff_vecs_t tracktest_vecs;
 extern ff_vecs_t ik3d_vecs;
 extern ff_vecs_t osm_vecs;
 extern ff_vecs_t destinator_poi_vecs;
@@ -935,6 +936,13 @@ vecs_t vec_list[] = {
                 "KML with Python",
                 "pykml"
 	},
+	{
+	        &tracktest_vecs,
+                "tracktest",
+                "Sample live tracking simulator",
+                "tracktest"
+	},
+
 	{
 		NULL,
 		NULL,
